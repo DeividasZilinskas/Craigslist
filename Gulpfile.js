1514e3dbc4e08c0ef1b41c7d565bd817c3378538
DeividasZilinskas <zilinskas.de@gmail.com>
2016-06-15 13:29:59 +0300

    Init npm, bower, configure gulp

---
+++ Gulpfile.js
@@ -0,0 +1,43 @@
+var gulp    = require('gulp');
+var sass    = require('gulp-sass');
+var concat  = require('gulp-concat');
+var uglify  = require('gulp-uglify');
+
+var dir = {
+    assets: './app/Resources/',
+    jsScripts: './app/Resources/scripts/',
+    dist: './web/',
+    bower: './bower_components/'
+};
+
+gulp.task('sass', function() {
+    gulp.src(dir.assets + 'style/main.scss')
+        .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
+        .pipe(concat('style.css'))
+        .pipe(gulp.dest(dir.dist + 'css'));
+});
+
+gulp.task('scripts', function() {
+    gulp.src([
+            dir.bower + 'jquery/dist/jquery.min.js'
+        ])
+        .pipe(concat('script.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest(dir.dist + 'js'));
+});
+
+gulp.task('images', function() {
+    gulp.src([
+            dir.assets + 'images/**'
+        ])
+        .pipe(gulp.dest(dir.dist + 'images'));
+});
+
+gulp.task('fonts', function() {
+    gulp.src([
+        dir.bower + 'bootstrap-sass/assets/fonts/**'
+        ])
+        .pipe(gulp.dest(dir.dist + 'fonts'));
+});
+
+gulp.task('default', ['sass', 'scripts', 'fonts', 'images']);
